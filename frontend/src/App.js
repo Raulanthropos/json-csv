af2225a041d76f764358a8eb5a2a34e265fd13a7
Raulanthropos <ipsichias@yandex.com>
2024-09-14 10:01:32 +0300

    Installed styled components & cleaned up unnecessary code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "react-tooltip/dist/react-tooltip.css";
-import { Tooltip } from "react-tooltip";
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [file, setFile] = useState(null);
   const [token, setToken] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -45,81 +42,8 @@ const App = () => {
     }
   }, [isTestUser]);
 
-  const handleTestLogin = async () => {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/login`,
-        { username: "test", password: "test" }
-      );
-      setToken(response.data.token);
-      localStorage.setItem("authToken", response.data.token); // Store token in localStorage
-      localStorage.setItem("username", "test");
-      setErrorMessage(""); // Clear error message on successful login
-    } catch (error) {
-      setErrorMessage("Login failed"); // Set error message on failure
-    }
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    setIsLoading(true);
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_API_URL}/login`,
-        { username, password }
-      );
-      setToken(response.data.token);
-      localStorage.setItem("authToken", response.data.token); // Store token in localStorage
-      setErrorMessage(""); // Clear error message on successful login
-    } catch (error) {
-      setErrorMessage("Login failed"); // Set error message on failure
-    }
-    setIsLoading(false);
-  };
-
-  const handleLogout = () => {
-    setToken("");
-    localStorage.removeItem("authToken"); // Remove token from localStorage
-    localStorage.removeItem("username");
-    setUsername("");
-    setPassword("");
-    setErrorMessage(""); // Clear error message on successful logout
-  };
-
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
-  };
-
-  const handleDownloadTestFile = async () => {
-    try {
-      setIsLoading(true); // Set loading state to true
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/download`,
-        {
-          responseType: "blob", // Important: ensures that the response is treated as a file
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("authToken")}`, // Make sure to send the authorization token
-          },
-        }
-      );
-
-      // Create a URL from the blob
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "testfile.json"); // Set the filename for the download
-      document.body.appendChild(link);
-      link.click();
-
-      // Clean up by revoking the URL and removing the link
-      window.URL.revokeObjectURL(url);
-      link.parentNode.removeChild(link);
-    } catch (error) {
-      console.error("Error downloading file:", error.message);
-      setErrorMessage("Failed to download file"); // Update error message state
-    }
-    setIsLoading(false); // Set loading state to false
   };
 
   const handleUpload = async (e) => {
